18d4d6f688c2b1f8a68f369051d9f078ce03ce1c
Kevin-Loran <kevinloran.eng@gmail.com>
2026-07-04 22:19:36 -0300

    feat: C++/Terminando exercicios.

--- C++/Exercicios_C++/Exercicio_DiagonalNegativo/main.cpp
+++ C++/Exercicios_C++/Exercicio_DiagonalNegativo/main.cpp
@@ -4,6 +4,30 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int m, i, j, contador;
+
+    cout << "Qual a ordem da matriz: ";
+    cin >> m;
+
+    int mat[m][m];
+
+    contador = 0;
+    for (i = 0; i < m; i++) {
+        for (j = 0; j < m; j++) {
+           cout << "Elemento [ " << i << ", " << j << "]: " ;
+           cin >> mat[i][j];
+
+           if (mat[i][j] < 0) {
+                contador = contador + 1;
+           }
+        }
+    }
+
+    cout << "diagonal principal: " << endl;
+    for (i = 0; i < m; i++) {
+        cout << " "<< mat[i][i];
+    }
+
+    cout << "\nQuantidades de numeros negativos: " << contador << endl;
     return 0;
 }
--- C++/Exercicios_C++/Exercicio_alturas/main.cpp
+++ C++/Exercicios_C++/Exercicio_alturas/main.cpp
@@ -4,6 +4,40 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int x, i, idade, contador;
+    double altura, alturaMedia, somaAltura, porcentagemIdade;
+    string nome;
+
+    cout << "Quantas pessoas serao digitadas: " << endl;
+    cin >> x;
+
+    string nomes[x];
+
+    somaAltura = 0;
+    for (i = 0; i < x; i++) {
+        cout << "\nDados da pessoa " << i+1 << ": " << endl;
+        cout << "nome: ";
+        cin.ignore(INT_MAX, '\n');
+        getline(cin, nome);
+        cout << "idade: ";
+        cin >> idade;
+        cout << "Altura: ";
+        cin >> altura;
+
+        somaAltura = somaAltura + altura;
+
+        if (idade < 16) {
+            nomes[i] = nome;
+            contador = contador + 1
+        }
+    }
+
+    alturaMedia = somaAltura / x;
+    porcentagemIdade = (contador / x) * 100;
+
+    for (i = 0; i < x; i++) {
+
+    }
+
     return 0;
 }
