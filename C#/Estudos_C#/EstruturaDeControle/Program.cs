ce85cfdeeea509fd4eb1e335119991a4db1b7114
Kevin-Loran <kevinloran.eng@gmail.com>
2026-07-09 00:32:09 -0300

    feat: C#/Aprendendo logica.

---
+++ C#/Estudos_C#/EstruturaDeControle/Program.cs
@@ -0,0 +1,16 @@
+﻿int hora;
+string resultado;
+
+Console.Write("Digite a hora: ");
+hora = int.Parse(Console.ReadLine());
+
+if (hora >= 6 && hora < 12) {
+    resultado = "bom-dia";
+}
+else if (hora > 12 && hora <= 18) {
+    resultado = "boa-tarde";
+} else {
+    resultado = "boa-noite";
+}
+
+Console.WriteLine(resultado);
