a15d2ceed7ac2099da7130ab4e509e17143ffa26
Kevin-Loran <kevinloran.eng@gmail.com>
2026-07-09 02:02:46 -0300

    Feat: C#/Exercicios.

---
+++ C#/Exercicios_C#/Exercicio_diagonalNegativo/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Qual a ordem da matriz: ");
+int m = int.Parse(Console.ReadLine());
+
+int[,] matriz = new int [m, m];
+
+int contador = 0;
+for (int i = 0; i< m; i++) {
+    for (int j = 0; j< m; j++) {
+        Console.WriteLine($"Elemento[{i}, {j}]: ");
+        matriz[i, j] = int.Parse(Console.ReadLine());
+        if (matriz[i, j] < 0) {
+            contador += 1;
+        }
+    }
+}
+
+Console.WriteLine("Diagonal Principal: ");
+for (int i = 0; i < m; i++) {
+     Console.Write(matriz[i, i]+  " ");
+}
+
+Console.WriteLine("Quantidade de negativo: " + contador);
