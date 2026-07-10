dae7354885d9a5d40321546c8cf00931d2d17dcf
Kevin-Loran <kevinloran.eng@gmail.com>
2026-07-10 00:08:33 -0300

    feat: Estudando python

---
+++ Python/Estudos_python/TiposDeDados.py
@@ -0,0 +1,10 @@
+nome = str("Maria Silva")
+idade = int(19)
+salario = float(13.470)
+genero = str('F')
+
+print(f"nome = {nome}")
+print(f"idade = {idade}")
+print(f"salario = {salario}")
+print(f"genero = {genero}")
+
